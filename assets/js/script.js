1ff7b8bce3960d53a557bf98bfe7e637823535c3
pratapsinhparmar <pratapsinh.work@gmail.com>
2024-01-02 11:03:48 +0530

    upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,7 @@
+var button = document.querySelector('.fabTrigger');
+var wrap = document.querySelector('.fabWidget');
 
-
-
-$( document ).ready(function() {
-    var arr = ['bg_1.jpg','bg_2.jpg','bg_3.jpg'];
-    
-    var i = 0;
-    setInterval(function(){
-        if(i == arr.length - 1){
-            i = 0;
-        }else{
-            i++;
-        }
-        var img = 'url(../assets/images/'+arr[i]+')';
-        $(".full-bg").css('background-image',img); 
-     
-    }, 4000)
-
-});
-
-
-
+button.onclick = function() {
+    button.classList.toggle('active');
+    wrap.classList.toggle('active');
+}
